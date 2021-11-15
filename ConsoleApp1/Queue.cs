6f43104a8233bbce5f37ffecfbd20fa9b439591c
Nick219 <nick219@yandex.ru>
2021-11-15 17:20:57 +0300

    Add project files.

---
+++ ConsoleApp1/Queue.cs
@@ -0,0 +1,70 @@
+﻿using System.Linq;
+
+namespace CircleQueue
+{
+    // Кольцевой буфер. Очередь (FIFO) на массиве фиксированного размера.
+    public class Queue<T>
+    {
+        private readonly T[] _array;
+        private readonly int _capacity;
+        private int _first = 0;
+        private int _last = 0;
+        private int _count = 0;
+
+        public Queue(int capacity)
+        {
+            _array = new T[capacity];
+            _capacity = capacity;
+        }
+
+        // Добавить элемент в массив
+        // `true` если удалось добавить элемент в очередь (ещё осталось место). В противном случае `false`
+        public bool Enq(T item)
+        {
+            int previousIndex = _last;
+
+            if (_count == _capacity)
+            {
+                return false;
+            }
+
+            lock (_array)
+            {
+                _last = (++_last) % _capacity;
+                _array[previousIndex] = item;
+                _count++;
+            }
+
+            return true;
+        }
+
+        // Извлечь элемент из массива
+        // возвращает `true` если очередь была не пустой и в out-параметре вернется значение первого в очереди элемента.
+        // `false` если очередь пуста и возвращать нечего.</returns>
+        public bool Deq(out T item)
+        {
+            item = default(T);
+            if (_count == 0)
+            {
+                return false;
+            }
+
+            item = _array[_first];
+            _array[_first] = default(T);
+            _first = (++_first) % _capacity;
+            _count--;
+
+            return true;
+        }
+
+        /// <summary>
+        /// Метод добавлен исключительно для тестирования
+        /// </summary>
+        /// <returns>Queue в виде копии внутреннего массива</returns>
+        public T[] GetQueueCopy()
+        {
+            T[] newarray = _array.ToArray();
+            return newarray;
+        }
+    }
+}
---
+++ TestQueue/QueueTest.cs
@@ -0,0 +1,114 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace TestQueue
+{
+    [TestClass]
+    public class QueueTest
+    {
+        private const int Capacity = 3;
+        private string str1 = "A";
+        private string str2 = "B";
+        private string str3 = "C";
+
+        [TestMethod]
+        public void ShouldEnqueueCorrectrly()
+        {
+            var q = new CircleQueue.Queue<string>(Capacity);
+
+            Assert.IsTrue(q.Enq(str1)); // true; A - -
+            Assert.IsTrue(q.Enq(str2)); // true; A B -
+            Assert.IsTrue(q.Enq(str3)); // true; A B C
+            Assert.IsFalse(q.Enq(str3));// false A B C
+            Assert.IsFalse(q.Enq(str2));// false A B C
+            Assert.IsFalse(q.Enq(str1));// false A B C
+
+            Assert.IsTrue(q.GetQueueCopy().SequenceEqual(new string[] { str1, str2, str3 }));
+        }
+
+        [TestMethod]
+        public void ShouldDequeueCorrectrly()
+        {
+            var q = new CircleQueue.Queue<string>(Capacity);
+            string result = string.Empty;
+            string empty = default(string);
+
+            Assert.IsFalse(q.Deq(out result));
+            Assert.IsFalse(q.Deq(out result));
+            Assert.IsFalse(q.Deq(out result));
+            Assert.IsFalse(q.Deq(out result));
+
+            Assert.IsTrue(q.Enq(str1)); // true; A - -
+            Assert.IsTrue(q.Enq(str2)); // true; A B -
+
+            Assert.IsTrue(q.Deq(out result)); // true; (A) B -
+            Assert.AreEqual(result, str1);
+            Assert.IsTrue(q.Deq(out result)); // true; - (B) -
+            Assert.AreEqual(result, str2);
+
+            Assert.IsFalse(q.Deq(out result)); // false; - - -
+
+            q.GetQueueCopy().ToList().ForEach(a => Console.WriteLine(a));
+            Assert.IsTrue(q.GetQueueCopy().SequenceEqual(new string[] { empty, empty, empty }));
+        }
+
+        /// <summary>
+        /// Тест помог мне найти баг в алгоритме
+        /// </summary>
+        [TestMethod]
+        public void RandomCalls()
+        {
+            var q = new CircleQueue.Queue<int>(Capacity);
+            int result = 0;
+            int testNumber = 5;
+            Random rnd = new Random();
+
+            for (int i = 0; i < 1000; i++)
+            {
+                int step = rnd.Next(2);
+                if (step == 0)
+                {
+                    q.Enq(testNumber);
+                }
+                else
+                {
+                    q.Deq(out result);
+                }
+            }
+
+            q.Deq(out result);
+            q.Deq(out result);
+            q.Deq(out result);
+
+            q.Enq(testNumber);
+            q.Deq(out result);
+
+            Assert.AreEqual(result, testNumber);
+        }
+
+        /// <summary>
+        /// Воспроизведение ситуации, в которой появлялась ошибка
+        /// </summary>
+        [TestMethod]
+        public void CoolTest()
+        {
+            var q = new CircleQueue.Queue<string>(Capacity);
+            string result = string.Empty;
+            q.Enq(str1);
+            q.Enq(str1);
+            q.Enq(str1);
+            q.Enq(str1);
+            q.Deq(out result);
+            q.Deq(out result);
+            q.Deq(out result);
+            q.Deq(out result);
+            q.Enq(str1);
+            q.Deq(out result);
+            Assert.AreEqual(result, str1);
+        }
+    }
+}
